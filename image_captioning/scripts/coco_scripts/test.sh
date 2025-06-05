e6480de81a4e8a77329768b8a1234987ee3bcba4
mongeoroo <30902020+mongeoroo@users.noreply.github.com>
2025-06-05 14:23:22 +0900

    Update test.sh

--- image_captioning/scripts/coco_scripts/test.sh
+++ image_captioning/scripts/coco_scripts/test.sh
@@ -1,8 +1,8 @@
 # PYTHONPATH 설정
-export PYTHONPATH=$PYTHONPATH:/mnt/MONG/C3/image_captioning
+export PYTHONPATH=$PYTHONPATH:path/to/image_captioning
 
 # 현재 디렉토리를 프로젝트 루트로 변경
-cd /mnt/MONG/C3/image_captioning
+cd path/to/image_captioning
 
 CUDA_VISIBLE_DEVICES=0 python3 run.py \
     --config configs/coco.yaml \
