b25061529f26760b3c5235d3260317a0b7f5a8a2
mongeoroo <30902020+mongeoroo@users.noreply.github.com>
2025-10-14 17:33:56 +0900

    Update script_inference.sh

--- ddpm/script_inference.sh
+++ ddpm/script_inference.sh
@@ -1,5 +1,5 @@
 #Diffusion augmentation
-CUDA_VISIBLE_DEVICES=0 python ddpm/make_dataset_with_diffusion_text.py --split train --weight_path results/model-300.pt --inference_step 600
+CUDA_VISIBLE_DEVICES=0 python ddpm/make_dataset_with_diffusion_text.py --split train --weight_path results/model-300.pt --inference_step 400
 
 #Diffusion Bridging validation and text set of vision embeddings
 CUDA_VISIBLE_DEVICES=0 python ddpm/make_dataset_with_diffusion_vision.py --split val --weight_path results/model-300.pt --inference_step 600
